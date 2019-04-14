ab1b39e22292094515fa2b2fbf14ba81a70de72d
rensramos <rensramos17@gmail.com>
2019-04-14 10:33:31 +0800

    Added carousel on about section

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ var swiper = new Swiper('.instagram-slider', {
     slidesPerView: 7,
     loop: true,
   });
+
+swiperStory = new Swiper('.slider-about', {
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+});
