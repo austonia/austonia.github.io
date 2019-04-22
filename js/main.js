851322ef5082c20a9250949eec2e630f7c13bd1b
rensramos <rensramos17@gmail.com>
2019-04-22 11:09:29 +0800

    Added scroll function and style on navbar

--- js/main.js
+++ js/main.js
@@ -1,12 +1,48 @@
-var swiper = new Swiper('.instagram-slider', {
+var austonia = {} || "";
+
+austonia.init = function(){
+    austonia.onNavbarScroll();
+    austonia.swiperCarousel();
+    // setTimeout(function(){
+    // }, 3000);
+}
+
+austonia.onNavbarScroll = function(){
+  var swiper = new Swiper('.instagram-slider', {
     slidesPerView: 7,
     loop: true,
   });
 
-swiperStory = new Swiper('.slider-about', {
-  slidesPerView: 1,
-  loop: true,
-  autoplay: {
-    delay: 3000,
-  },
+  swiperStory = new Swiper('.slider-about', {
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+  });
+}
+
+austonia.swiperCarousel = function(){
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 30 && $(document).width() >= 992) {
+        $('nav').addClass('navbar-shrink');
+    }
+  
+    else if ($(document).width() <= 991){
+        $('nav').addClass('navbar-shrink');
+    }
+    else {
+        $('nav').removeClass('navbar-shrink');
+    }
+  });
+  
+  if ($(document).width() <= 991) {
+    $('nav').addClass('navbar-shrink');
+  }
+}
+
+$(function(){
+  austonia.init();
 });
+
+
